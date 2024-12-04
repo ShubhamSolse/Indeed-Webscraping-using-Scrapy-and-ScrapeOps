b375eb0554b0d76a1556a544ae35861dad1ac25c
ShubhamSolse <shubhamsolse@gmail.com>
2024-12-04 09:41:15 +0000

    Commiting the updated file on 04-Dec-2024. added comments on scrapingindeed.py file

--- scrapingindeed/scrapingindeed/spiders/scrapingindeed.py
+++ scrapingindeed/scrapingindeed/spiders/scrapingindeed.py
@@ -1,30 +1,36 @@
-import re
-import json
+# importing required libraries
+import re  # The `re` library handles pattern-matching and text manipulation.
+import json  # The `json` library handles encoding/decoding JSON data in Python.
 from typing import Any
-import scrapy
-from requests import Response
-import requests
-
-from ..items import ScrapingindeedItem
+import scrapy  # The 'scrapy' library used to scrape website
+from requests import Response  # `Response` from `requests` handles HTTP responses from web server requests.
+from ..items import ScrapingindeedItem  # importing the 'ScrapingindeedItem' class from items.py file
 
 
+# Code written to create a class for scraping indeed website
 class IndeedScraping(scrapy.Spider):
-    name = "indeedscraper"
-    next_page_number = 10
+    name = "indeedscraper"  # Name of the scrapy spider
+    next_page_number = 10  # Varaiable used for pagination
     start_urls = [
         'https://ie.indeed.com/jobs?q=python&l=Dublin&start=0'
-    ]
+    ]  # start_urls store the staring url for scraping
 
+    # code has been written to create a method to parse the website and scrape the data
     def parse(self, response: Response, **kwargs: Any):
-
+        # Initializing the ScrapyindeedItem class
         items = ScrapingindeedItem()
 
+        # Extracts JSON-like data from specific JavaScript in HTML response.
         script_tag = re.findall(
             r'window.mosaic.providerData\["mosaic-provider-jobcards"\]=(\{.+?\});',
             response.text)
+        # Converts the first extracted JSON string into a Python dictionary.
         json_blob = json.loads(script_tag[0])
+
+        # navigating to the results where the job data to be scraped is present
         jobs_list = json_blob["metaData"]['mosaicProviderJobCardsModel']['results']
 
+        # code written to extract the required data form job_list
         for index, job in enumerate(jobs_list):
             if job.get('jobkey') is not None:
                 Company = job.get('company')
@@ -71,9 +77,9 @@ class IndeedScraping(scrapy.Spider):
 
                 yield items
 
+        # Below code is written for pagination
         next_page = f"https://ie.indeed.com/jobs?q=python&l=Dublin&start={str(self.next_page_number)}"
 
         if IndeedScraping.next_page_number < 210:
             IndeedScraping.next_page_number += 10
             yield response.follow(next_page, callback=self.parse)
-
