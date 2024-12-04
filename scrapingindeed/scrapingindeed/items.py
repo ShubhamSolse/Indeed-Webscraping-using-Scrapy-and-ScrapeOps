f11a3a137ac8758055788c31ac34f98412585876
ShubhamSolse <shubhamsolse@gmail.com>
2024-12-04 09:12:42 +0000

    Commiting the updated file on 04-Dec-2024. added comments on items.py file

--- scrapingindeed/scrapingindeed/items.py
+++ scrapingindeed/scrapingindeed/items.py
@@ -3,8 +3,8 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+# Code written to process data which has been scraped from scrapingindeed.py file
 import scrapy
-
 
 class ScrapingindeedItem(scrapy.Item):
     Company = scrapy.Field()
